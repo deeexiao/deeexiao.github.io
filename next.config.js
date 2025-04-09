2d13cffb28c0bd1e0c31ac39c0d148dcd746c7aa
D. Xiao <dxiao@MaydeMBP.home>
2025-04-09 05:47:53 +0200

    Fix GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
   trailingSlash: true,
 }
 
