a582ba0aa87e74101b96dc979d9df5018f6b4cc7
deeexiao <dxiao@MaydeMBP.home>
2025-04-16 12:52:29 +0200

    Fix static assets path configuration

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
     domains: ['assets.super.so', 'images.spr.so'],
   },
   trailingSlash: true,
+  basePath: '/deeexiao.github.io',
+  assetPrefix: '/deeexiao.github.io/',
 }
 
 module.exports = nextConfig 
